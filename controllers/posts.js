5f9d18c1ba2aa4e16bc08eee12d0e7f1d22faa12
ZachBarcoe <z.barcoe@gmail.com>
2021-06-19 15:56:39 +0000

    post update added

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,3 +1,5 @@
+import mongoose from "mongoose";
+import { TrainRounded } from "../../client/node_modules/@material-ui/icons/index.js";
 import PostMessage from "../models/postMessage.js";
 
 const handle = (err) => console.log(err);
@@ -16,4 +18,16 @@ export const createPosts = async (req, res) => {
         .save()
         .then(() => res.status(201).json(newPost))
         .catch((err) => res.status(409).json({ message: err.message }));
+};
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(_id))
+        return res.status(404).send("No Post with that id");
+
+    await PostMessage.findByIdAndUpdate(_id, post, { new: TrainRounded }).then(
+        (updatedPost) => res.status(200).json(updatedPost)
+    );
 };
