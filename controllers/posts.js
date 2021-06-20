ffedc31543197143c653476d6d4f4ab09490f86f
ZachBarcoe <z.barcoe@gmail.com>
2021-06-20 00:59:04 +0000

    likes now array of user ids

--- controllers/posts.js
+++ controllers/posts.js
@@ -47,14 +47,21 @@ export const deletePost = async (req, res) => {
 export const likePost = async (req, res) => {
     const { id: _id } = req.params;
 
+    if (!req.userId) return res.status(401);
+
     if (!mongoose.Types.ObjectId.isValid(_id))
         return res.status(404).send("No Post with that id");
 
-    await PostMessage.findById(_id).then(async (post) => {
-        await PostMessage.findByIdAndUpdate(
-            _id,
-            { ...post, likeCount: post.likeCount++ },
-            { new: true }
-        ).then((updatedPost) => res.status(200).json(updatedPost));
-    });
+    if (post.likes)
+        await PostMessage.findById(_id).then(async (post) => {
+            post.likes.findIndex((id) => id === String(req.userId)) === -1
+                ? (post.likes = post.likes.push(req.userId))
+                : (post.likes = post.likes.filter(
+                      (id) => id !== String(req.userId)
+                  ));
+
+            await PostMessage.findByIdAndUpdate(_id, post, { new: true }).then(
+                (updatedPost) => res.status(200).json(updatedPost)
+            );
+        });
 };
