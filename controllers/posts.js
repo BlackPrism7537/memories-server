203eeda9bd3965a95b7ad403d3a5db2c393bc810
ZachBarcoe <z.barcoe@gmail.com>
2021-06-20 20:08:29 +0000

    posts updated

--- controllers/posts.js
+++ controllers/posts.js
@@ -11,7 +11,12 @@ export const getPosts = async (req, res) => {
 
 export const createPosts = async (req, res) => {
     const post = req.body;
-    const newPost = new PostMessage(post);
+
+    const newPost = new PostMessage({
+        ...post,
+        creator: req.userId,
+        createdAt: new Date().toISOString(),
+    });
 
     await newPost
         .save()
@@ -45,23 +50,24 @@ export const deletePost = async (req, res) => {
 };
 
 export const likePost = async (req, res) => {
-    const { id: _id } = req.params;
+    const { id } = req.params;
 
     if (!req.userId) return res.status(401);
 
-    if (!mongoose.Types.ObjectId.isValid(_id))
+    if (!mongoose.Types.ObjectId.isValid(id))
         return res.status(404).send("No Post with that id");
 
-    if (post.likes)
-        await PostMessage.findById(_id).then(async (post) => {
-            post.likes.findIndex((id) => id === String(req.userId)) === -1
-                ? (post.likes = post.likes.push(req.userId))
-                : (post.likes = post.likes.filter(
-                      (id) => id !== String(req.userId)
-                  ));
+    const post = await PostMessage.findById(id);
 
-            await PostMessage.findByIdAndUpdate(_id, post, { new: true }).then(
-                (updatedPost) => res.status(200).json(updatedPost)
-            );
-        });
+    const index = post.likes.findIndex((id) => id === String(req.userId));
+
+    if (index === -1) {
+        post.likes.push(req.userId);
+    } else {
+        post.likes = post.likes.filter((id) => id !== String(req.userId));
+    }
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, {
+        new: true,
+    });
+    res.status(200).json(updatedPost);
 };
